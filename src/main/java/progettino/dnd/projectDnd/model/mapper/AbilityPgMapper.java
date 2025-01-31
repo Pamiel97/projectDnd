0574c604e8eaf86c12fb44645bbb2f4bdfb8b711
Pasquale Ciccarelli <pasqualeciccarelli20@gmail.com>
2025-01-31 15:47:00 +0100

    Repository

--- src/main/java/progettino/dnd/projectDnd/model/mapper/AbilityPgMapper.java
+++ src/main/java/progettino/dnd/projectDnd/model/mapper/AbilityPgMapper.java
@@ -2,14 +2,16 @@ package progettino.dnd.projectDnd.model.mapper;
 
 import org.mapstruct.Mapper;
 
+import org.mapstruct.Mapping;
 import progettino.dnd.projectDnd.dtos.AbilityPgDto;
 import progettino.dnd.projectDnd.model.entities.AbilityPg;
 
 
 @Mapper(componentModel = "spring")
 public interface AbilityPgMapper {
-
-    AbilityPg toEntity(AbilityPgDto abilityPgDto);
-
+    @Mapping(source = "ability", target = "abilityDto")
     AbilityPgDto toDto(AbilityPg abilityPg);
+
+    @Mapping(source = "abilityDto", target = "ability")
+    AbilityPg toEntity(AbilityPgDto abilityPgDto);
 }
